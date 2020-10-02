90359d382ca9df5cb124bfa805f127822bc7fd64
Rafael <rafaelcorreamendes@hotmail.com>
2020-10-02 01:33:41 -0300

    dropbox

---
+++ dropbox-api/src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <button @click="list">carrega</button>
+
+  <ul id="example-1">
+  <li v-for="item in files" :key="item.name">
+    {{ item.name }}
+  </li>
+</ul>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+const { Dropbox } = require('../teste/dropbox');
+
+var files = [{}];
+
+export default {
+  name: 'Home',
+  
+data: ()=> {
+    return {
+      files
+    }
+  },
+
+  methods:{
+    list(){
+
+
+ const dbx = new Dropbox({ accessToken: 'fTlXC_o880AAAAAAAAAAAdof2yxz-P2K8XIm4z3aMkcNRn8DEXKf-zaGhRefTSWo' });     
+
+      dbx.filesListFolder({path: '/win'})
+        .then(function(response) {
+          console.log('response', response.entries)
+           
+          this.files = response.entries;
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+
+
+
+    },
+  }
+}
+</script>
